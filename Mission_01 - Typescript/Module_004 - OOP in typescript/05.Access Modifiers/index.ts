e1ce6322ef52c8c3e7ca0ffc42f2fe9b0eaa1d38
ASSiddik01 <assiddik001@gmail.com>
2023-05-06 13:18:04 +0600

    M004 V05 - Access Modifiers

--- Mission_01 - Typescript/Module_004 - OOP in typescript/05.Access Modifiers/index.ts
+++ Mission_01 - Typescript/Module_004 - OOP in typescript/05.Access Modifiers/index.ts
@@ -6,18 +6,13 @@ class BankAccount {
   constructor(
     readonly id: number,
     public name: string,
-    private _balance: number
+    public balance: number
   ) {}
 
   getBalance() {
-    console.log(`My current balance is ${this._balance} `);
-  }
-  addDeposite(amount: number) {
-    this._balance = this._balance + amount;
+    console.log(`My current balance is ${this.balance} `);
   }
 }
 
 const myAccount = new BankAccount(11212, "Shama", 3000);
-
-myAccount.addDeposite(3000);
 myAccount.getBalance();
