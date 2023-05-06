2fea0f281fba4c476f8b7c67898cfd3552224d92
ASSiddik01 <assiddik001@gmail.com>
2023-05-06 14:30:57 +0600

    M004 V09 - Abstraction

--- Mission_01 - Typescript/Module_004 - OOP in typescript/00.TS_Server/index.ts
+++ Mission_01 - Typescript/Module_004 - OOP in typescript/00.TS_Server/index.ts
@@ -1,62 +1,44 @@
-class Person {
-  takeNap(): void {
-    console.log(`I am sleeping 8 hours per day`);
+// abstraction interface
+interface IVehicel {
+  startEngine(): void;
+  move(): void;
+  stopEngine(): void;
+}
+
+class Vehicel implements IVehicel {
+  startEngine(): void {
+    console.log("start");
+  }
+  move(): void {
+    console.log("move");
+  }
+  stopEngine(): void {
+    console.log("stop");
   }
 }
 
-class Student extends Person {
-  takeNap(): void {
-    console.log(`I am sleeping 10 hours per day`);
+const car = new Vehicel();
+car.startEngine();
+
+// abstraction class
+
+abstract class Vehicel1 {
+  abstract startEngine(): void;
+  abstract move(): void;
+  abstract stopEngine(): void;
+}
+
+class Car extends Vehicel1 {
+  startEngine(): void {
+    console.log("car start");
+  }
+  move(): void {
+    console.log("car move");
+  }
+  stopEngine(): void {
+    console.log("car stop");
   }
 }
 
-class Developer extends Person {
-  takeNap(): void {
-    console.log(`I am sleeping 5 hours per day`);
-  }
-}
-
-function getNap(param: Person) {
-  param.takeNap();
-}
-
-const person = new Person();
-const student = new Student();
-const developer = new Developer();
-
-getNap(person);
-getNap(student);
-getNap(developer);
-
-class Shape {
-  getArea(): number {
-    return 0;
-  }
-}
-
-class Circle extends Shape {
-  constructor(public radius: number) {
-    super();
-  }
-
-  getArea(): number {
-    return Math.PI * this.radius * this.radius;
-  }
-}
-
-class Rectangle extends Shape {
-  constructor(public height: number, public width: number) {
-    super();
-  }
-
-  getArea(): number {
-    return this.height * this.width;
-  }
-}
-
-function getAreaOfShape(param: Shape) {
-  console.log(param.getArea());
-}
-
-getAreaOfShape(new Circle(12));
-getAreaOfShape(new Rectangle(12, 12));
+const newCar = new Car();
+newCar.startEngine();
