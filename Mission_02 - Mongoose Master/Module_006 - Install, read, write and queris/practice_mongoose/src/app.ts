1a1a8d29d33fc096e3aa121c6a173428bc070592
ASSiddik01 <assiddik001@gmail.com>
2023-05-14 12:05:29 +0600

    M006 V04 - Interface and schema

--- Mission_02 - Mongoose Master/Module_006 - Install, read, write and queris/practice_mongoose/src/app.ts
+++ Mission_02 - Mongoose Master/Module_006 - Install, read, write and queris/practice_mongoose/src/app.ts
@@ -2,11 +2,91 @@ import express, { Application, Request, Response } from "express";
 import cors from "cors";
 import colors from "colors";
 const app: Application = express();
+import { Schema, model, connect } from "mongoose";
+
+// Middleware
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Testing API
 app.get("/", (req: Request, res: Response) => {
   res.send("+++ App running Successfully +++");
+});
+
+// Testing API
+app.get("/user", (req: Request, res: Response) => {
+  //   create user interface
+  interface IUser {
+    id: string;
+    role: string;
+    name: {
+      firstName: string;
+      lastName: string;
+    };
+    password: string;
+    dateOfBirth?: string;
+    gender: "male" | "female";
+    email?: string;
+    contactNo: string;
+    emargencyContactNo: string;
+    presentAddress: string;
+    permanentAddress: string;
+  }
+
+  // 2. Create a Schema corresponding to the document interface.
+  const userSchema = new Schema<IUser>({
+    id: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    role: {
+      type: String,
+      required: true,
+    },
+    name: {
+      firstName: {
+        type: String,
+        required: true,
+      },
+      lastName: {
+        type: String,
+        required: true,
+      },
+    },
+    dateOfBirth: {
+      type: String,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    gender: {
+      type: String,
+      enum: ["male", "female"],
+      required: true,
+    },
+    email: {
+      type: String,
+    },
+    contactNo: {
+      type: String,
+      required: true,
+    },
+    emargencyContactNo: {
+      type: String,
+      required: true,
+    },
+    presentAddress: {
+      type: String,
+      required: true,
+    },
+    permanentAddress: {
+      type: String,
+      required: true,
+    },
+  });
 });
 
 // Unknown API Handle
