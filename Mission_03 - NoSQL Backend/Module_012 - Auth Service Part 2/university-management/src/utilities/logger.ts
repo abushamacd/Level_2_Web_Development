1e791c99e3720d984839f15e07be45c51de8be41
ASSiddik01 <assiddik001@gmail.com>
2023-06-02 16:31:40 +0600

    M12 V06 - Customize the log

--- Mission_03 - NoSQL Backend/Module_012 - Auth Service Part 2/university-management/src/utilities/logger.ts
+++ Mission_03 - NoSQL Backend/Module_012 - Auth Service Part 2/university-management/src/utilities/logger.ts
@@ -1,25 +1,32 @@
 /* eslint-disable no-undef */
-import winston from 'winston'
+import { createLogger, format, transports } from 'winston'
+const { combine, timestamp, label, printf } = format
 import path from 'path'
 
-export const logger = winston.createLogger({
+const myFormat = printf(({ level, message, label, timestamp }) => {
+  const date = new Date(timestamp)
+
+  return `${date} - [${label}] ${level}: ${message}`
+})
+
+export const logger = createLogger({
   level: 'info',
-  format: winston.format.json(),
+  format: combine(label({ label: 'UMS' }), timestamp(), myFormat),
   transports: [
-    new winston.transports.Console(),
-    new winston.transports.File({
+    new transports.Console(),
+    new transports.File({
       filename: path.join(process.cwd(), 'logs', 'winston', 'success.log'),
       level: 'info',
     }),
   ],
 })
 
-export const errorLogger = winston.createLogger({
+export const errorLogger = createLogger({
   level: 'error',
-  format: winston.format.json(),
+  format: combine(label({ label: 'UMS' }), timestamp(), myFormat),
   transports: [
-    new winston.transports.Console(),
-    new winston.transports.File({
+    new transports.Console(),
+    new transports.File({
       filename: path.join(process.cwd(), 'logs', 'winston', 'error.log'),
       level: 'error',
     }),
