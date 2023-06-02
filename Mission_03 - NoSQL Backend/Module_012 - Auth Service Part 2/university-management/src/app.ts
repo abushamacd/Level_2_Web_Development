666be24400a809797a827a102b5ab7ba91350cfb
ASSiddik01 <assiddik001@gmail.com>
2023-06-02 18:16:04 +0600

    M12 V09 - Different ways to handle error

--- Mission_03 - NoSQL Backend/Module_012 - Auth Service Part 2/university-management/src/app.ts
+++ Mission_03 - NoSQL Backend/Module_012 - Auth Service Part 2/university-management/src/app.ts
@@ -1,4 +1,4 @@
-import express, { Application, Request, Response } from 'express'
+import express, { Application, Request, Response, NextFunction } from 'express'
 import cors from 'cors'
 import userRoute from '../src/app/modules/users/user.route'
 const app: Application = express()
@@ -11,9 +11,39 @@ app.use(express.urlencoded({ extended: true }))
 // Data API
 app.use('/api/v1/user', userRoute)
 
+// error
+class ApiError extends Error {
+  // statusCode: number
+  constructor(
+    public statusCode: number,
+    public message: string | undefined,
+    public stack = ''
+  ) {
+    super(message)
+    this.statusCode = statusCode
+    if (stack) {
+      this.stack = stack
+    } else {
+      Error.captureStackTrace(this, this.constructor)
+    }
+  }
+}
+
 // Testing API
-app.get('/', (req: Request, res: Response) => {
-  res.send('+++ App Running Successfully +++')
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+  // res.send('+++ App Running Successfully +++')
+  // throw new ApiError(403, 'test')
+  throw new Error('test')
+  // next('Error ar Error')
+})
+
+// Global error handle
+app.use((err, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof Error) {
+    res.status(400).json({ error: err })
+  } else {
+    res.status(404).json({ error: 'Something Wrong' })
+  }
 })
 
 // Unknown API Handle
