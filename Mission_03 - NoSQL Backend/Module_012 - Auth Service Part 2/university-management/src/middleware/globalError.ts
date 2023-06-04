f91bf0cd6b53e3905f276cd90621e00dc8566cae
ASSiddik01 <assiddik001@gmail.com>
2023-06-04 22:24:53 +0600

    M12 V13 - Refactor global error handler

--- Mission_03 - NoSQL Backend/Module_012 - Auth Service Part 2/university-management/src/middleware/globalError.ts
+++ Mission_03 - NoSQL Backend/Module_012 - Auth Service Part 2/university-management/src/middleware/globalError.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from 'express'
 import config from '../config'
 import { IErrorMessage } from '../interface/error'
 import { handleValidationError } from './handleValidationError'
+import { ApiError } from './apiError'
 
 export const globarError = (
   err: any,
@@ -9,13 +10,37 @@ export const globarError = (
   res: Response,
   next: NextFunction
 ) => {
-  const statusCode = 400
-  const message = 'Something went wrong'
-  const errorMessage: IErrorMessage[] = []
+  let statusCode = 400
+  let message = 'Something went wrong'
+  let errorMessage: IErrorMessage[] = []
 
   //
   if (err?.name === 'ValidationError') {
     const simplifiedError = handleValidationError(err)
+    statusCode = simplifiedError.statusCode
+    message = simplifiedError.message
+    errorMessage = simplifiedError.errorMessage
+  } else if (err instanceof ApiError) {
+    statusCode = err?.statusCode
+    message = err?.message
+    errorMessage = err?.message
+      ? [
+          {
+            path: '',
+            message: err?.message,
+          },
+        ]
+      : []
+  } else if (err instanceof Error) {
+    message = err?.message
+    errorMessage = err?.message
+      ? [
+          {
+            path: '',
+            message: err?.message,
+          },
+        ]
+      : []
   }
 
   //
