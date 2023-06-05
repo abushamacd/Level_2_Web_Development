1e3cbfa19f7697ea649cae2f0ea4e8f6691324cb
ASSiddik01 <assiddik001@gmail.com>
2023-06-05 14:58:21 +0600

    M13 V05 - Zod validation

--- Mission_03 - NoSQL Backend/Module_013 - Error Handleinn, Zod, Semester Model/university-management/src/app/modules/users/user.controller.ts
+++ Mission_03 - NoSQL Backend/Module_013 - Error Handleinn, Zod, Semester Model/university-management/src/app/modules/users/user.controller.ts
@@ -1,20 +1,8 @@
 import { RequestHandler } from 'express'
 import { createUserService } from './user.services'
-import { z } from 'zod'
 
 export const createUser: RequestHandler = async (req, res, next) => {
   try {
-    // const userZod = z.object({
-    //   body: z.object({
-    //     role: z.string({
-    //       required_error: 'Z: Role is required',
-    //     }),
-    //     password: z.string().optional(),
-    //   }),
-    // })
-
-    // await userZod.parseAsync(req)
-
     const result = await createUserService(req.body)
     res.status(200).send({
       success: true,
--- Mission_03 - NoSQL Backend/Module_013 - Error Handleinn, Zod, Semester Model/university-management/src/app/modules/users/user.route.ts
+++ Mission_03 - NoSQL Backend/Module_013 - Error Handleinn, Zod, Semester Model/university-management/src/app/modules/users/user.route.ts
@@ -1,7 +1,9 @@
 import express from 'express'
 import { createUser } from './user.controller'
+import { userZod } from './user.validation'
+import reqValidate from './../../../middleware/reqValidate'
 const router = express.Router()
 
-router.route('/').post(createUser)
+router.route('/').post(reqValidate(userZod), createUser)
 
 export default router
