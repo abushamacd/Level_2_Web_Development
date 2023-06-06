46b10e4a69dfe08169c727d5711a36606c7b4c7e
shama <assiddik001@gmail.com>
2023-06-06 18:33:36 +0600

    M13 V14 - Optimize Try-Catch Block, Api Response

--- Mission_03 - NoSQL Backend/Module_013 - Error Handleinn, Zod, Semester Model/university-management/src/app/modules/academicSemester/acaSem.controller.ts
+++ Mission_03 - NoSQL Backend/Module_013 - Error Handleinn, Zod, Semester Model/university-management/src/app/modules/academicSemester/acaSem.controller.ts
@@ -1,12 +1,18 @@
-import { Request, Response } from 'express'
+import { NextFunction, Request, Response } from 'express'
 import { createAcaSemService } from './acaSem.services'
 import { tryCatch } from '../../../utilities/tryCatch'
+import { sendRes } from '../../../utilities/sendRes'
+import status from 'http-status'
 
-export const createAcaSem = tryCatch(async (req: Request, res: Response) => {
-  const result = await createAcaSemService(req.body)
-  res.status(200).send({
-    success: true,
-    message: 'Academic Semester successfully',
-    result: result,
-  })
-})
+export const createAcaSem = tryCatch(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const result = await createAcaSemService(req.body)
+    next()
+    sendRes(res, {
+      statusCode: status.OK,
+      success: true,
+      message: 'Academic Semester successfully',
+      result: result,
+    })
+  }
+)
--- Mission_03 - NoSQL Backend/Module_013 - Error Handleinn, Zod, Semester Model/university-management/src/utilities/tryCatch.ts
+++ Mission_03 - NoSQL Backend/Module_013 - Error Handleinn, Zod, Semester Model/university-management/src/utilities/tryCatch.ts
@@ -1,9 +1,9 @@
-import { NextFunction, Request, Response } from 'express'
+import { NextFunction, Request, RequestHandler, Response } from 'express'
 
-export const tryCatch = fn => {
+export const tryCatch = (fn: RequestHandler) => {
   return async (req: Request, res: Response, next: NextFunction) => {
     try {
-      fn(req, res)
+      await fn(req, res, next)
     } catch (error) {
       next(error)
     }
