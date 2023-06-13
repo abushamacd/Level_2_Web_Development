3dc6371b1167662eae28dad50ee98266c90d3f14
ASSiddik01 <assiddik001@gmail.com>
2023-06-13 12:40:05 +0600

    M15 V09 - Create User-Student use Transac Rollback

--- Mission_03 - NoSQL Backend/Module_015 - Complate Student Module, Transection, Rollback/university-management/src/app/modules/users/user.services.ts
+++ Mission_03 - NoSQL Backend/Module_015 - Complate Student Module, Transection, Rollback/university-management/src/app/modules/users/user.services.ts
@@ -1,9 +1,13 @@
+import mongoose from 'mongoose'
 import config from '../../../config'
 import { AcaSem } from '../academicSemester/acaSem.model'
 import { IStudent } from '../student/student.interface'
 import { IUser } from './user.interface'
 import { User } from './user.model'
 import { generateStudentId } from './user.utils'
+import { Student } from '../student/student.model'
+import status from 'http-status'
+import { ApiError } from '../../../errorFormating/apiError'
 
 export const createStudentService = async (
   student: IStudent,
@@ -18,14 +22,36 @@ export const createStudentService = async (
 
   const academicsemester = await AcaSem.findById(student.academicSemester)
 
-  const id = await generateStudentId(academicsemester)
-  user.id = id
-  student.id = id
+  let newUserAllData = null
+  const session = await mongoose.startSession()
+  try {
+    session.startTransaction()
+    const id = await generateStudentId(academicsemester)
+    user.id = id
+    student.id = id
+
+    // create student
+    const newStudent = await Student.create([student], { session })
+    if (!newStudent.length) {
+      throw new ApiError(status.BAD_REQUEST, 'Failed to create student')
+    }
+
+    //set student -->  _id into user.student
+    user.student = newStudent[0]._id
+    const newUser = await User.create([user], { session })
+
+    if (!newUser.length) {
+      throw new ApiError(status.BAD_REQUEST, 'Failed to create user')
+    }
+    newUserAllData = newUser[0]
+
+    await session.commitTransaction()
+    await session.endSession()
+  } catch (error) {
+    await session.abortTransaction()
+    await session.endSession()
+    throw error
+  }
 
   //
-  const result = await User.create(user)
-  if (!result) {
-    throw new Error('User create failed')
-  }
-  return result
 }
