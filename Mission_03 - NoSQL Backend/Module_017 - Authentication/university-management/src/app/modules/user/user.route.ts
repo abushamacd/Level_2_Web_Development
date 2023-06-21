e174a744478f84a47cd7a9b54aa8ca2225d50a25
shama <assiddik001@gmail.com>
2023-06-21 19:12:14 +0600

    M17 V01 - Previous Practice

---
+++ Mission_03 - NoSQL Backend/Module_017 - Authentication/university-management/src/app/modules/user/user.route.ts
@@ -0,0 +1,25 @@
+import express from 'express';
+import validateRequest from '../../middlewares/validateRequest';
+import { UserController } from './user.controller';
+import { StudentValidaion } from '../student/student.validation';
+import { FacultyValidaion } from '../faculty/faculty.validation';
+import { AdminValidation } from '../admin/admin.validation';
+const router = express.Router();
+
+router.post(
+  '/create-student',
+  validateRequest(StudentValidaion.createUserZodSchema),
+  UserController.createStudent
+);
+router.post(
+  '/create-faculty',
+  validateRequest(FacultyValidaion.createFacultyZodSchema),
+  UserController.createFacultyMember
+);
+router.post(
+  '/create-admin',
+  validateRequest(AdminValidation.createAdminZodSchema),
+  UserController.createAdmin
+);
+
+export const UserRoutes = router;
---
+++ Mission_03 - NoSQL Backend/Module_017 - Authentication/university-management/src/interfaces/common.ts
@@ -0,0 +1,25 @@
+import { IGenericErrorMessage } from './error';
+
+export type IGenericResponse<T> = {
+  meta: {
+    page: number;
+    limit: number;
+    total: number;
+  };
+  data: T;
+};
+
+export type IGenericErrorResponse = {
+  statusCode: number;
+  message: string;
+  errorMessages: IGenericErrorMessage[];
+};
+
+export type ICommonFilters = {
+  searchTerm?: string;
+  id?: string;
+  bloodGroup?: string;
+  email?: string;
+  contactNo?: string;
+  emergencyContactNo?: string;
+};
