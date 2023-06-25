1f53198b6528966ca5ebf8134ad32553d3837f0c
shama <assiddik001@gmail.com>
2023-06-25 10:34:48 +0600

    M17 V15 - Verify token using auth middleware

--- Mission_03 - NoSQL Backend/Module_017 - Authentication/university-management/src/app/modules/user/user.route.ts
+++ Mission_03 - NoSQL Backend/Module_017 - Authentication/university-management/src/app/modules/user/user.route.ts
@@ -4,22 +4,26 @@ import { UserController } from './user.controller';
 import { StudentValidaion } from '../student/student.validation';
 import { FacultyValidaion } from '../faculty/faculty.validation';
 import { AdminValidation } from '../admin/admin.validation';
+import auth from '../../middlewares/auth';
+import { ENUM_USER_ROLE } from '../../../enums/user';
 const router = express.Router();
 
 router.post(
   '/create-student',
   validateRequest(StudentValidaion.createUserZodSchema),
-  // auth(),
+  auth(ENUM_USER_ROLE.ADMIN),
   UserController.createStudent
 );
 router.post(
   '/create-faculty',
   validateRequest(FacultyValidaion.createFacultyZodSchema),
+  auth(ENUM_USER_ROLE.ADMIN),
   UserController.createFacultyMember
 );
 router.post(
   '/create-admin',
   validateRequest(AdminValidation.createAdminZodSchema),
+  auth(ENUM_USER_ROLE.ADMIN),
   UserController.createAdmin
 );
 
