5d13213306688f8187dacbe2beab019e23148c25
shama <assiddik001@gmail.com>
2023-07-06 10:15:23 +0600

    M20 V07 -Connect component with redux store

--- Mission_04 - Redux Rapper/Module_20 - Redux Philoshpy/tech-net/src/page/Home.tsx
+++ Mission_04 - Redux Rapper/Module_20 - Redux Philoshpy/tech-net/src/page/Home.tsx
@@ -1,5 +1,21 @@
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../redux/store";
+
 const Home = () => {
-  return <div>Home</div>;
+  const { count } = useSelector((state: RootState) => state.counter);
+  const dispatch = useDispatch();
+
+  return (
+    <div className="flex gap-2 justify-center items-center">
+      <button className="p-2 border border-green-400 rounded-md">
+        Increment
+      </button>
+      <div>{count}</div>
+      <button className="p-2 border border-red-400 rounded-md">
+        Decrement
+      </button>
+    </div>
+  );
 };
 
 export default Home;
