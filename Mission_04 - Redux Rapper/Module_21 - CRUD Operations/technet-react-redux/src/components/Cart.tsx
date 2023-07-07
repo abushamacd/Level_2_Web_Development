ea22cff9d96fff48db743b1e2837359d0f4a2e9e
shama <assiddik001@gmail.com>
2023-07-07 15:46:56 +0600

    M21 V03 - Handling business logic

--- Mission_04 - Redux Rapper/Module_21 - CRUD Operations/technet-react-redux/src/components/Cart.tsx
+++ Mission_04 - Redux Rapper/Module_21 - CRUD Operations/technet-react-redux/src/components/Cart.tsx
@@ -13,11 +13,11 @@ import {
 } from 'react-icons/hi';
 import { Button } from './ui/button';
 import { IProduct } from '@/types/globalTypes';
+import { useAppSelector } from '@/redux/hooks';
 
 export default function Cart() {
+  const { products } = useAppSelector((state) => state.cart);
   //! Dummy data
-
-  const products: IProduct[] = [];
   const total = 0;
 
   //! **
@@ -44,9 +44,9 @@ export default function Cart() {
                 <img src={product?.image} alt="" className="h-full" />
               </div>
               <div className="px-2 w-full flex flex-col gap-3">
-                <h1 className="text-2xl self-center">{product?.name}</h1>
+                <h1 className="text-xl self-center">{product?.name}</h1>
                 <p>Quantity: {product.quantity}</p>
-                <p className="text-xl">
+                <p className="text-sm">
                   Total Price: {(product.price * product.quantity!).toFixed(2)}{' '}
                   $
                 </p>
