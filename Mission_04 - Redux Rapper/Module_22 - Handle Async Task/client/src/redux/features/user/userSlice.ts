4eeac6eba87f871e3a886c7b18b99d21f0df67ea
shama <assiddik001@gmail.com>
2023-07-10 14:01:00 +0600

    M22 V06 - Async operation handle

--- Mission_04 - Redux Rapper/Module_22 - Handle Async Task/client/src/redux/features/user/userSlice.ts
+++ Mission_04 - Redux Rapper/Module_22 - Handle Async Task/client/src/redux/features/user/userSlice.ts
@@ -1,4 +1,6 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { auth } from '@/lib/firebase';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
 
 interface IUser {
   user: {
@@ -8,6 +10,11 @@ interface IUser {
   isError: boolean;
   isSuccess: boolean;
   error: null | string;
+}
+
+interface ICredentials {
+  email: string;
+  password: string;
 }
 
 const initialState: IUser = {
@@ -20,10 +27,45 @@ const initialState: IUser = {
   error: null,
 };
 
+const createUser = createAsyncThunk(
+  'user',
+  async ({ email, password }: ICredentials) => {
+    const data = await createUserWithEmailAndPassword(auth, email, password);
+    // .then((userCredential) => {
+    //   // Signed in
+    //   const user = userCredential.user;
+    //   // ...
+    // })
+    // .catch((error) => {
+    //   const errorCode = error.code;
+    //   const errorMessage = error.message;
+    //   // ..
+    // });
+    return data?.user?.email;
+  }
+);
+
 const userSlice = createSlice({
   name: 'user',
   initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(createUser.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(createUser.fulfilled, (state) => {
+        state.isLoading = false;
+        state.isError = false;
+        state.user.email = null;
+      })
+      .addCase(createUser.rejected, (state) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.user.email = null;
+        state.error;
+      });
+  },
 });
 
 // export const { } = userSlice.actions;
