5fb5922013d234cfe46d771e45794db6cb82b145
shama <assiddik001@gmail.com>
2023-07-21 18:58:20 +0600

    M25 V03 - Revalidate of getStaticProps()

--- Mission_05 - NextJS Ninja/Module_25 - Data Fatching in NextJS/news_portal/src/pages/index.js
+++ Mission_05 - NextJS Ninja/Module_25 - Data Fatching in NextJS/news_portal/src/pages/index.js
@@ -30,10 +30,10 @@ HomePage.getLayout = function getLayout(page) {
 export const getStaticProps = async () => {
   const res = await fetch("http://localhost:5000/news");
   const data = await res.json();
-  console.log(data);
   return {
     props: {
       allNews: data,
     },
+    revalidate: 5,
   };
 };
