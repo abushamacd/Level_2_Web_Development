083cfb64694a60cb3eba3409d1c8ac33b3c4dedf
shama <assiddik001@gmail.com>
2023-08-09 06:20:02 +0600

    M30 V06 - Create, Update, Delete A Table

---
+++ Mission_06 - SQL Superstar/Module_30 - SQL Introductions/intro.sql
@@ -0,0 +1,14 @@
+-- Active: 1691426787114@@127.0.0.1@5432@new_db
+
+--  ## create TABLE
+-- CREATE TABLE "user" (
+--     user_id INT,
+--     email VARCHAR(50),
+--     phone VARCHAR(15)
+-- );
+
+-- ## update table name
+-- ALTER TABLE "user" RENAME TO "users";
+
+-- ## delete TABLE
+-- DROP TABLE "users"
