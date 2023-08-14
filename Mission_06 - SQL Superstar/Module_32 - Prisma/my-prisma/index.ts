001d905fea344e2630948cf7fb7c15d61cca17a4
shama <assiddik001@gmail.com>
2023-08-14 10:45:46 +0600

    M32 V05 - Migrate and generate

--- Mission_06 - SQL Superstar/Module_32 - Prisma/my-prisma/index.ts
+++ Mission_06 - SQL Superstar/Module_32 - Prisma/my-prisma/index.ts
@@ -3,17 +3,17 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  //   const user = await prisma.user.create({
-  //     data: {
-  //       email: "Siddik@gmail.com",
-  //       name: "Siddik",
-  //     },
-  //   });
+  const user = await prisma.user.create({
+    data: {
+      email: "Shabbir@gmail.com",
+      name: "Shabbir",
+      age: 23,
+    },
+  });
+  console.log(user);
 
-  //   console.log(user);
-
-  const users = await prisma.user.findMany();
-  console.log(users);
+  //   const users = await prisma.user.findMany();
+  //   console.log(users);
 }
 
 main();
