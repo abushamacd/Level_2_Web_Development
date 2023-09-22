f86ae897e3a4171c4cb8a183ba5d73edac7a0048
Shama <assiddik001@gmail.com>
2023-09-22 16:29:34 +0600

    M46 V05 - Adding Zod Validations

--- Mission_06 - SQL Superstar/Module_45 - API Getway P1/UM_API_getway/src/app/modules/academicSemester/academicSemester.routes.ts
+++ Mission_06 - SQL Superstar/Module_45 - API Getway P1/UM_API_getway/src/app/modules/academicSemester/academicSemester.routes.ts
@@ -8,6 +8,7 @@ import { AcademicSemesterController } from './academicSemster.controller';
 const router = express.Router();
 
 router.get('/', AcademicSemesterController.getAllFromDB);
+router.get('/:id', AcademicSemesterController.getByIdFromDB);
 
 router.post(
   '/',
@@ -21,6 +22,12 @@ router.patch(
   validateRequest(AcademicSemesterValidation.update),
   auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
   AcademicSemesterController.updateOneIntoDB
+);
+
+router.delete(
+  '/:id',
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  AcademicSemesterController.deleteByIdFromDB
 );
 
 export const academicSemesterRoutes = router;
