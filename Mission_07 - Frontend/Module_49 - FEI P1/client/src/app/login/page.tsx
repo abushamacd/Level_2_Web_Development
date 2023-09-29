824f341b2032f10d9806c505b622fd5dfe45d7ea
Shama <assiddik001@gmail.com>
2023-09-29 11:02:36 +0600

    M50 V06 - Private routes

--- Mission_07 - Frontend/Module_49 - FEI P1/client/src/app/login/page.tsx
+++ Mission_07 - Frontend/Module_49 - FEI P1/client/src/app/login/page.tsx
@@ -7,6 +7,7 @@ import FormInput from "@/components/forms/FormInput";
 import { SubmitHandler } from "react-hook-form";
 import { useUserLoginMutation } from "@/redux/api/authApi";
 import { saveUserInfo } from "@/service/auth.service";
+import { useRouter } from "next/navigation";
 
 type FormValues = {
   id: string;
@@ -14,11 +15,14 @@ type FormValues = {
 };
 
 const LoginPage = () => {
+  const router = useRouter();
   const [userLogin] = useUserLoginMutation();
   const onSubmit: SubmitHandler<FormValues> = async (data: any) => {
     try {
       const res = await userLogin({ ...data }).unwrap();
-      // console.log(res);
+      if (res?.data.accessToken) {
+        router.push("/profile");
+      }
       saveUserInfo({ accessToken: res?.data.accessToken });
     } catch (err: any) {
       console.error(err.message);
